33e3d20bbffed51bd321cd6e920acf1f391ca344
Friction <freshwolf4@gmail.com>
2025-12-16 21:27:14 +0530

    hopeful fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  turbopack: false, // Disable Turbopack to fix textract build error
 };
 
 export default nextConfig;
